32aa5f0d65db68f7a7b29dbf4d0f508fd88fedb1
johanwk <johan.w.kluwer@gmail.com>
2024-03-24 13:05:39 +0100

    lstdefinelanguage

--- elot-package/elot-defaults.el
+++ elot-package/elot-defaults.el
@@ -5,7 +5,6 @@
  org-export-allow-bind-keywords t
  org-babel-default-inline-header-args '((:exports . "code"))
  org-latex-src-block-backend 'listings
- org-latex-classes '(("book" "\\documentclass[10pt]{scrbook}" ontology-resource-section))
  org-latex-prefer-user-labels t
  org-latex-image-default-scale .8
  time-stamp-line-limit 100
@@ -16,6 +15,17 @@
  org-startup-folded 'show2levels
  org-export-with-sub-superscripts nil  ; preserve "_"
  org-export-headline-levels 8  ; deep numbering
+ org-latex-packages-alist
+ (append org-latex-packages-alist '(
+   ("" "svg" t)
+   ("" "enumitem" t)
+   "\\setlist[description]{font=\\normalfont\\itshape\\space}"
+   "\\sloppy"
+   "\\hypersetup{pdfborder=0 0 0}"
+   "\\lstdefinelanguage{omn}{basicstyle=\\small\\ttfamily,commentstyle=\\color{gray},frame=single,breaklines=true,breakatwhitespace=true,postbreak=\\mbox{{\\color{gray}\\tiny$\\rightarrow$}},tabsize=2,comment=[l]{\\#},columns=fullflexible,}"
+   "\\lstdefinelanguage{ttl}{basicstyle=\\footnotesize\\ttfamily,commentstyle=\\color{gray},frame=single,breaklines=true,breakatwhitespace=true,postbreak=\\mbox{{\\color{gray}\\tiny$\\rightarrow$}},tabsize=2,comment=[l]{\\#},columns=fullflexible,}"
+   "\\lstdefinelanguage{sparql}{basicstyle=\\footnotesize\\ttfamily,commentstyle=\\color{gray},frame=single,breaklines=true,breakatwhitespace=true,postbreak=\\mbox{{\\color{gray}\\tiny$\\rightarrow$}},tabsize=2,comment=[l]{\\#},columns=fullflexible,}"
+   ))
  )
 (progn
   (require 'elot)
