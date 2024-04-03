9095efcfaf49a9025876a8f7fddea324545ba62b
johanwk <johan.w.kluwer@gmail.com>
2024-04-03 17:57:06 +0200

    some cleanup

--- elot-init.el
+++ elot-init.el
@@ -14,33 +14,96 @@
 (package-initialize)
 (package-refresh-contents)
 
+;;; The following may be necessary for package installation in Windows
+;;; (if a problem with gpg key for installing packages)
+;;; create manually the directory .emacs.d/elpa/gnupg
+;;; before installing packages
+;;; https://emacs.stackexchange.com/questions/60278/gpg-no-public-key
+;;;
+;; (make-directory "~/.emacs.d/elpa/gnupg" :parents)
+;; (setq package-gnupghome-dir "~/.emacs.d/elpa/gnupg")
+
+
+;; various packages
 (dolist (package '(htmlize
                    omn-mode
-                   ;; apparently not needed org-tempo
-                   modus-themes  ;; good looks
-                   orderless ;; easier to find commands
+                   sparql-mode
+                   ob-plantuml
+                   org-ql
+                   ;; the following are completely optional
+                   modus-themes
+                   general
+                   orderless
+                   vertico
+                   marginalia
+                   consult
                    ))
  (unless (package-installed-p package)
    (package-install package)
    (require package)))
 
 (require 'ob-lob) ; Library of Babel
+(require 'ob-sparql)
 (require 'ox) ; export functions
 (require 'ol) ; link functions
-
 (setq tempo-interactive t)
-(load-theme 'modus-operandi-tritanopia t)
 
 ;; location of the elot package -- edit path to fit
 (push (expand-file-name "~/elisp/elot-package/") load-path)
 (load-library "elot")
 
+(org-babel-do-load-languages
+      'org-babel-load-languages
+      '((emacs-lisp . t) (plantuml . t)))
 
-;; only emacs-lisp is needed for minimal setup
-;; (org-babel-do-load-languages
-;;       'org-babel-load-languages
-;;       '((emacs-lisp . t) (plantuml . t)))
+;; Queries/statistics and diagrams in ELOT rely on external software
+;; programs, which need to be installed separately.
+;;
+;; the following variables then need to be edited with M-x customize
+;; - elot-robot-jar-path    (for ROBOT query and more)
+;; - org-plantuml-jar-path  (for PlantUML)
+;; - elot-rdfpuml-path      (for RDF diagrams)
 
-;; only needed when we get to rdfpuml
-;; need direct pointer to plantuml
-(setq org-plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
+
+
+
+;; ----------------------------------------------------------------------
+;;  The following is entirely optional. Gives a reasonable setup for
+;;  new Emacs users.
+;;  ----------------------------------------------------------------------
+
+(load-theme 'modus-operandi-tritanopia t)
+
+(use-package general)  ; a dependency
+
+(use-package vertico
+  :init
+  (vertico-mode +1))
+
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+
+(use-package marginalia
+  :config (marginalia-mode))
+
+(use-package consult
+  :general
+  ("M-y" 'consult-yank-from-kill-ring
+   "C-x b" 'consult-buffer
+   "M-g i" 'consult-imenu))
+
+(recentf-mode)
+
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
+
